ffa0ceca79d59cafc750c69c1d70dd9015460039
Romuald Lemesle <romu.lemesle@gmail.com>
2026-05-25 16:49:56 +0200

    [implant] fix(security): log response status instead of full response object

--- src/api/manage_inject.rs
+++ src/api/manage_inject.rs
@@ -152,7 +152,12 @@ impl Client {
         retry: u64,
     ) -> Result<UpdateInjectResponse, Error> {
         if response.status().is_success() {
-            info!("response {response:?} to update status for inject id: {inject_id:?} and agent id: {agent_id:?}");
+            info!(
+                "response {} to update status for inject id: {:?} and agent id: {:?}",
+                response.status(),
+                inject_id,
+                agent_id
+            );
             response
                 .json::<UpdateInjectResponse>()
                 .map_err(|e| Error::Internal(e.to_string()))
